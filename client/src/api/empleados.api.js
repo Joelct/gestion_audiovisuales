567378b0e2631d02ea2effc047fb86dfa1783a18
Joel Camilo <joelctline@gmail.com>
2024-06-05 00:19:58 -0400

    Arreglos del cuarto commit"

--- client/src/api/empleados.api.js
+++ client/src/api/empleados.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/marcas';
+const API_URL = 'http://localhost:3000/api/empleados';
 
 export const getEmpleadosRequest = async () => {
   try {
